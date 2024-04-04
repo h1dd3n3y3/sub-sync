0909c6a357801447f18c166bab2c0ebc12c1d9ca
tru3w1tn3ss <parathyrasdavid@gmail.com>
2024-04-04 12:10:31 +0300

    Updates & fixes

--- sub_sync.sh
+++ sub_sync.sh
@@ -4,10 +4,14 @@ check_installation() {
     if [ -z "$(which "$1")" ]; then
         echo "'bc' tool is essential for running the script."
         read -p "\nDo you wish to install it right now? [Y/n] " ans
+
         if [ -z "$ans" ] || [ "$ans" = "Y" ] || [ "$ans" = "y" ]; then
-            apt install "$1" -y
+            sudo apt install "$1" -y
         elif [ "$ans" = "N" ] || [ "$ans" = "n" ]; then
-            exit
+            exit 1
+        else # Invalid input
+            echo "Invalid input. Please try again."
+            check_installation "$1"
         fi
     fi
 }
@@ -18,6 +22,7 @@ ts_to_sec() {
     mm=${ts:3:2}
     ss=${ts:6:2}
     ms=0.${ts:9:3}
+
     totsec0=$((10#$hh * 3600 + 10#$mm * 60 + 10#$ss)) # Avoid bash confusion by forcing base ten with 10#
     totsec=$(echo "$totsec0 + $ms" | bc)
 }
@@ -45,6 +50,7 @@ sec_to_ts() {
 dialogue() {
     old=$1
     new=$2
+
     clear
     echo -e "\e[36m +-----------------------------+-----------------------------+\e[0m\n"\
         "\e[36m|\e[93m Old timestamp: \e[94m$old \e[36m|\e[93m New timestamp: \e[94m$new \e[36m|\e[0m\n"\
@@ -53,6 +59,7 @@ dialogue() {
 
 percentage() {
     prog=$1
+
     [ "${prog:0:1}" = "." ] && newprog=0           # Take care the percentage form because `bc -l`
     [ "${prog:1:1}" = "." ] && newprog=${prog:0:1} # division results are displayed without
     [ "${prog:2:1}" = "." ] && newprog=${prog:0:2} # leading zero if the result is lower than 1
@@ -64,29 +71,51 @@ check_installation "bc" # Check and install 'bc'
 
 if [ $# -ge 2 ]; then
     args=( "$@" )
+
     for a in "${!args[@]}"; do
-        [ "${args[$a]}" = "--silent" ] || [ "${args[$a]}" = "-s" ] && silent="true"
-        [[ "${args[$a]}" =~ \.srt$ ]] && subs=${args[$a]} && n=0\
-            && totts=$(grep -o '[0-5][0-9]:[0-5][0-9]:[0-5][0-9],[0-9][0-9][0-9]' "$subs" | wc -l)
-        [[ "${args[$a]}" =~ ^[+-]?[0-9]+([.][0-9]+)?$ ]] &&timeoff=${args[$a]}\
-            && [ "${timeoff:0:1}" = "+" ] && timeoff=${timeoff:1} # remove '+' from the offset
+        if [ "${args[$a]}" = "--silent" ] || [ "${args[$a]}" = "-s" ]; then # Silent mode
+            silent="true"
+        fi
+
+        if [[ "${args[$a]}" =~ \.srt$ ]]; then
+            subs=${args[$a]}
+
+            cp "$subs" "${subs}_$(date +"%Y-%m-%d_%H-%M-%S")" # Backup the original subtitle file
+            
+            n=0
+            totts=$(grep -o '[0-5][0-9]:[0-5][0-9]:[0-5][0-9],[0-9][0-9][0-9]' "$subs" | wc -l)
+        fi
+        
+        if [[ "${args[$a]}" =~ ^[+-]?[0-9]+([.][0-9]+)?$ ]]; then
+            timeoff=${args[$a]}
+
+            if [ "${timeoff:0:1}" = "+" ]; then
+                timeoff=${timeoff:1} # remove '+' from the offset
+            fi
+        fi
     done
+
     echo $timeoff
+
     for i in $(grep -o '[0-5][0-9]:[0-5][0-9]:[0-5][0-9],[0-9][0-9][0-9]' "$subs"); do
         ts_to_sec "$i"                  # Convert the extracted timestamp to seconds
         offset "$timeoff"               # Add the given offset seconds to the previously converted timestamp
         sec_to_ts                       # Convert the new seconds back to timestamp
         sed -i "s/$i/$newts/" "$subs"   # Replace old timestamp with the new one
+
         if ! [ $silent ]; then
-	    echo "Processing..."
             ((n++))
             progress=$(echo "$n / $totts * 100" | bc -l)
             progress=${progress:0:3}
             dialogue "$i" "$newts"
             percentage "$progress"
+        else
+	        echo "Processing..."
         fi
     done
+
     echo "Done."
+
 elif [ $# -lt 2 ]; then
     echo "Try '$0 [OPTION] <subtitles> <offset>'"
     echo -e "\nOPTION:\n\t-s, --silent: Suppress graphical progress."
